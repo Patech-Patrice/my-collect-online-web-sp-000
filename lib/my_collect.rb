3b77b747ebc398cc7d613f6d17710e9994aae7c1
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:30:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(collection)
+  
+  
+  
+  
+  
+  
+  
+  
+  ["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
