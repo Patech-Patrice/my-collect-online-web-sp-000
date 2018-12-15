7624da8af14a6302af8bbd305a75bb028d026315
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:41:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,7 @@
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   empty_array = []
-  
-  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
 
-["Tim Jones", "Tom Smith", "Jim Campagno"].each do | collection |
    i = 0
  
   while i < array.length
@@ -18,8 +12,6 @@ end
   
   # call your method here!
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-
  my_collect(array) do |name|
   name.split(" ").first
 end
