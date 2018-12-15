6a76e27296b6f9813eb77e90ffdce36170499f1a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:42:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   empty_array = []
-
    i = 0
- 
   while i < array.length
+  yield(array[i])
     i = i + 1
   end
 end
