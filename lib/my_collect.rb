bce7f7361cd3bbaac8db4b7d8e54f257e7888a60
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:32:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,12 @@
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(collection)
   
+  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+["Tim Jones", "Tom Smith", "Jim Campagno"].each do | collection |
   
   
   
