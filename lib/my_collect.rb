6d9d0b43b26cc633e779a70271f2675803d34ac9
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-15 23:36:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -17,12 +17,11 @@ end
   
   
   # call your method here!
-array = ["Tim", "Tom", "Jim"]
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}" #We're calling our method with the array of names as an argument and accompanying that method call with a block that accepts a |name| parameter. If the passed-in name begins with the letter "T", the block will puts out a greeting
-  end
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(["Tim", "Tom", "Jim"]) do |name|
+ my_collect(array) do |name|
+  name.split(" ").first
+end
 end
 
   
